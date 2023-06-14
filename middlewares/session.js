2971d255a972f1cc56b1ee289896800c70e757ae
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-06-14 14:59:35 +0300

    Add session middleware

---
+++ middlewares/session.js
@@ -0,0 +1,20 @@
+const { verifyToken } = require("../services/userServices");
+
+
+module.exports = () => (req, res, next) => {
+    const token = req.cookies.token;
+
+    if (token) {
+        try {
+            const userData = verifyToken(token);
+            req.user = userData;
+           
+            res.locals.username = userData.firstName;
+        
+        } catch (err) {
+            res.clearCookie('token');
+            res.redirect('/auth/login');
+        }
+    }
+    next();
+}
